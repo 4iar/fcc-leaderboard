c4ddd98dd39370b1652ba705f555c45a9d12b956
4iar <4iar@users.noreply.github.com>
2016-07-06 20:24:24 +0100

    Add endpoint handling

---
+++ src/constants/endpoints.js
@@ -0,0 +1,1 @@
+export const LEADERBOARD_API_ENDPOINT = "https://fcctop100.herokuapp.com/api/fccusers/top/recent";
--- src/containers/LeaderboardPage.js
+++ src/containers/LeaderboardPage.js
@@ -1,9 +1,28 @@
 import React from 'react';
 
+import Table from '../components/Table';
+import { LEADERBOARD_API_ENDPOINT } from '../constants/endpoints';
+
 export default class LeaderboardPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      users: []
+    };
+  }
+
+  componentDidMount() {
+    this.request = $.get(LEADERBOARD_API_ENDPOINT, function (result) {
+      this.setState({
+        users: result
+      });
+    }.bind(this));
+  }
+
   render() {
+    console.log(this.state.users);
     return (
-      <p>Hello World!</p>
+      <Table data={this.state.users} />
     );
   }
 }
